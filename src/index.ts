a68ef123577188c741d8af20e788bc60cc52cb05
Filip Troníček <filip@gitpod.io>
2021-11-25 21:06:39 +0000

    More functions

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ const fs = require('fs');
 
 const licenseData: License[] = JSON.parse(fs.readFileSync('./src/resources/licenses.json', 'utf8'));
 
-export const checkLicense = (askedFor: string) => {
-    const queriedLicense = licenseData.find((lic) => askedFor === lic.id)
-    return queriedLicense;
+export const checkShorthand = (askedFor: string) => {
+    const queriedLicense = licenseData.find((license) => askedFor === license.id)
+    return !!queriedLicense;
 }
 
-console.log(checkLicense("MIT"))
+export const checkFullName = (askedFor: string) => {
+    const queriedLicense = licenseData.find((license) => askedFor === license.id)
+    return !!queriedLicense;
+}
