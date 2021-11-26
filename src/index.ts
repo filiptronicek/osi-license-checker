d7c3f4793a2b44d305a4d3123d0009c97ab5bd3e
Filip Troníček <filip@gitpod.io>
2021-11-26 18:53:46 +0000

    jsdoc

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import fs from 'fs';
 
 const licenseData: License[] = JSON.parse(fs.readFileSync(`${__dirname}/../src/resources/licenses.json`, 'utf8'));
 
+/**
+ * Checks whether the provided license is an OSI-approved SPDX License Identifier
+ * @param askedFor the license string to query
+ * @param additionalLicenses an array of licenses to check in addition to the standard list found in https://github.com/filiptronicek/osi-license-checker/blob/main/src/resources/licenses.json
+ */
 export const checkShorthand = (askedFor: string, additionalLicenses: License[] = []) => {
     if (askedFor === "") return false;
     const combinedLicenses = [...licenseData, ...additionalLicenses];
@@ -9,6 +14,11 @@ export const checkShorthand = (askedFor: string, additionalLicenses: License[] =
     return !!queriedLicense;
 }
 
+/**
+ * Checks whether the provided license is an OSI-approved License name
+ * @param askedFor the license string to query
+ * @param additionalLicenses an array of licenses to check in addition to the standard list found in https://github.com/filiptronicek/osi-license-checker/blob/main/src/resources/licenses.json
+ */
 export const checkFullName = (askedFor: string, additionalLicenses: License[] = []) => {
     const combinedLicenses = [...licenseData, ...additionalLicenses];
     const queriedLicense = combinedLicenses.find((license) => askedFor === license.name);
