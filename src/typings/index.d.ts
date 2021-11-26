5ba891deab5c9cd9c822f9c840fda4004fdfdf0e
Filip Troníček <filip@gitpod.io>
2021-11-26 08:38:36 +0000

    Typings

---
+++ src/typings/index.d.ts
@@ -0,0 +1,14 @@
+interface License {
+    /**
+     * The full name of the license
+     * @example MIT License
+     * @example Apache License 2.0
+     */
+    fullName: string;
+    /**
+     * The SPDX License Identifier
+     * @example MIT
+     * @example Apache-2.0
+     */
+    id: string;
+}
